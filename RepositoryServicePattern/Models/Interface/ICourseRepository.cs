0f0ec9f49b5979d8200eb3edf7166e4e4edb4537
chuangmaster <chuang.master@gmail.com>
2018-01-06 18:43:13 +0800

    加入ICourseRepository；加入IPersonRepository

---
+++ RepositoryServicePattern/Models/Interface/ICourseRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RepositoryServicePattern.Models.Interface
+{
+    public interface ICourseRepository
+    {
+        void Create(Course instance);
+
+        void Update(Course instance);
+
+        void Delete(Course instance);
+
+        Course Get(int categoryID);
+
+        IQueryable<Course> GetAll();
+
+        void SaveChanges();
+    }
+}
---
+++ RepositoryServicePattern/Models/Interface/IPersonRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RepositoryServicePattern.Models.Interface
+{
+    public interface IPersonRepository
+    {
+        void Create(Person instance);
+
+        void Update(Person instance);
+
+        void Delete(Person instance);
+
+        Person Get(int categoryID);
+
+        IQueryable<Person> GetAll();
+
+        void SaveChanges();
+    }
+}
