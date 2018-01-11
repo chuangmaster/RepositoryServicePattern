7315d9dcdb561fb9d824a6814f99b3789cb1888d
chuangmaster <chuang.master@gmail.com>
2018-01-12 00:57:11 +0800

    加入 GenericRepository與 IRepository

---
+++ RepositoryServicePattern/Models/GenericRepository.cs
@@ -0,0 +1,99 @@
+﻿using RepositoryServicePattern.Models.Interface;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Core.Objects;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Web;
+
+namespace RepositoryServicePattern.Models
+{
+    public class GenericRepository<TEntity> : IRepository<TEntity> where TEntity : class
+    {
+        private DbContext _context
+        {
+            get;
+            set;
+        }
+        public GenericRepository() : this(new ContosoUniversityEntities())
+        {
+
+        }
+        public GenericRepository(DbContext context)
+        {
+            if (context == null)
+            {
+                throw new ArgumentNullException("context");
+            }
+            this._context = context;
+        }
+        //這個建構子看不懂
+        public GenericRepository(ObjectContext context)
+        {
+            if (context == null)
+            {
+                throw new ArgumentNullException("context");
+            }
+            this._context = new DbContext(context, true);
+        }
+        public void Create(TEntity instance)
+        {
+            if (instance == null)
+            {
+                throw new ArgumentNullException("instance");
+            }
+            else
+            {
+                this._context.Set<TEntity>().Add(instance);
+                this.SaveChanges();
+            }
+        }
+
+        public void Delete(TEntity instance)
+        {
+            if (instance == null)
+            {
+                throw new ArgumentNullException("instance");
+            }
+            else
+            {
+                this._context.Entry(instance).State = EntityState.Deleted;
+                this.SaveChanges();
+            }
+        }
+
+
+        public TEntity Get(Expression<Func<TEntity, bool>> predicate)
+        {
+            return this._context.Set<TEntity>().FirstOrDefault(predicate);
+        }
+
+        public IQueryable<TEntity> GetAll()
+        {
+            return this._context.Set<TEntity>().AsQueryable();
+        }
+
+        public void SaveChanges()
+        {
+            this._context.SaveChanges();
+        }
+
+        public void Update(TEntity instance)
+        {
+            if (instance == null)
+            {
+                throw new ArgumentNullException("instance");
+            }
+            else
+            {
+                this._context.Entry(instance).State = EntityState.Modified;
+                this.SaveChanges();
+            }
+        }
+        public void Dispose()
+        {
+            
+        }
+    }
+}
