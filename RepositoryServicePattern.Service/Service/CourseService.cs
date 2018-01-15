dc0af6f48c54cfd064550551174e7ae386bb887d
chuangmaster <chuang.master@gmail.com>
2018-01-16 01:41:28 +0800

    加入Service層

---
+++ RepositoryServicePattern.Service/Service/CourseService.cs
@@ -0,0 +1,95 @@
+﻿using RepositoryServicePattern.Repository;
+using RepositoryServicePattern.Repository.Interface;
+using RepositoryServicePattern.Repository.Models;
+using RepositoryServicePattern.Service.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RepositoryServicePattern.Service.Service
+{
+    public class CourseService : ICourseService
+    {
+        private IRepository<Course> repository = new GenericRepository<Course>();
+
+
+        public IResult Create(Course instance)
+        {
+            if (instance == null)
+            {
+                throw new ArgumentNullException();
+            }
+
+            IResult result = new Result(false);
+            try
+            {
+                this.repository.Create(instance);
+                result.Success = true;
+            }
+            catch (Exception ex)
+            {
+                result.Exception = ex;
+            }
+            return result;
+        }
+
+        public IResult Update(Course instance)
+        {
+            if (instance == null)
+            {
+                throw new ArgumentNullException();
+            }
+
+            IResult result = new Result(false);
+            try
+            {
+                this.repository.Update(instance);
+                result.Success = true;
+            }
+            catch (Exception ex)
+            {
+                result.Exception = ex;
+            }
+            return result;
+        }
+
+        public IResult Delete(int courseID)
+        {
+            IResult result = new Result(false);
+
+            if (!this.IsExists(courseID))
+            {
+                result.Message = "找不到資料";
+            }
+
+            try
+            {
+                var instance = this.GetByID(courseID);
+                this.repository.Delete(instance);
+                result.Success = true;
+            }
+            catch (Exception ex)
+            {
+                result.Exception = ex;
+            }
+            return result;
+        }
+
+        public bool IsExists(int courseID)
+        {
+            return this.repository.GetAll().Any(x => x.CourseID == courseID);
+        }
+
+        public Course GetByID(int courseID)
+        {
+            return this.repository.Get(x => x.CourseID == courseID);
+        }
+
+        public IEnumerable<Course> GetAll()
+        {
+            return this.repository.GetAll();
+        }
+    }
+}
---
+++ RepositoryServicePattern.Service/Service/PersonService.cs
@@ -0,0 +1,95 @@
+﻿using RepositoryServicePattern.Repository;
+using RepositoryServicePattern.Repository.Interface;
+using RepositoryServicePattern.Repository.Models;
+using RepositoryServicePattern.Service.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RepositoryServicePattern.Service.Service
+{
+    public class PersonService: IPersonService
+    {
+        private IRepository<Person> repository = new GenericRepository<Person>();
+
+
+        public IResult Create(Person instance)
+        {
+            if (instance == null)
+            {
+                throw new ArgumentNullException();
+            }
+
+            IResult result = new Result(false);
+            try
+            {
+                this.repository.Create(instance);
+                result.Success = true;
+            }
+            catch (Exception ex)
+            {
+                result.Exception = ex;
+            }
+            return result;
+        }
+
+        public IResult Update(Person instance)
+        {
+            if (instance == null)
+            {
+                throw new ArgumentNullException();
+            }
+
+            IResult result = new Result(false);
+            try
+            {
+                this.repository.Update(instance);
+                result.Success = true;
+            }
+            catch (Exception ex)
+            {
+                result.Exception = ex;
+            }
+            return result;
+        }
+
+        public IResult Delete(int ID)
+        {
+            IResult result = new Result(false);
+
+            if (!this.IsExists(ID))
+            {
+                result.Message = "找不到資料";
+            }
+
+            try
+            {
+                var instance = this.GetByID(ID);
+                this.repository.Delete(instance);
+                result.Success = true;
+            }
+            catch (Exception ex)
+            {
+                result.Exception = ex;
+            }
+            return result;
+        }
+
+        public bool IsExists(int ID)
+        {
+            return this.repository.GetAll().Any(x => x.ID == ID);
+        }
+
+        public Person GetByID(int ID)
+        {
+            return this.repository.Get(x => x.ID == ID);
+        }
+
+        public IEnumerable<Person> GetAll()
+        {
+            return this.repository.GetAll();
+        }
+    }
+}
